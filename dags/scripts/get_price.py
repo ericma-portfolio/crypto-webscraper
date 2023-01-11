7f73913f25b5234f09aaba47b662fd033d46cdd1
Eric Ma <ericma@Erics-MBP.lan>
2023-01-11 17:50:22 -0600

    linter fixes

--- dags/scripts/get_price.py
+++ dags/scripts/get_price.py
@@ -2,37 +2,32 @@ import sys
 from requests import Request, Session
 import json
 import os
-sys.path.insert(0,os.path.abspath(os.path.dirname('/Users/ericma/desktop/cryptoScraper')))
 from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 
-def get_price() :
-    url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+def get_price():
+    url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest"
     # get top 10 currencies based by market cap
-    parameters = {
-        'start' : '1',
-        'limit' : '10', 
-        'convert' : 'USD'
-    }
+    parameters = {"start" : "1", "limit" : "10", "convert" : "USD"}
 
     headers = {
-        'Accepts': 'application/json',
-        'X-CMC_PRO_API_KEY' : os.getenv('COIN_MARKET_CAP_API')
+        "Accepts": "application/json",
+        "X-CMC_PRO_API_KEY" : os.getenv("COIN_MARKET_CAP_API")
     }
 
     session = Session()
     session.headers.update(headers)
 
-    response = session.get(url, params = parameters)
-    #return coins in json format
-    coins = json.loads(response.text)['data']
+    response = session.get(url, params=parameters)
+    # return coins in json format
+    coins = json.loads(response.text)["data"]
 
     price = []
 
     for coin in coins:
 
-        data = coin['symbol'], coin['quote']['USD']['price']
+        data = coin["symbol"], coin["quote"]["USD"]["price"]
         
         price.append(data)
 
--- dags/upload_to_s3.py
+++ dags/upload_to_s3.py
@@ -1,6 +1,9 @@
 import sys
 import os
-sys.path.insert(0,os.path.abspath(os.path.dirname('/Users/ericma/desktop/cryptoScraper/dags/scripts')))
+
+sys.path.insert(
+    0,os.path.abspath(os.path.dirname('/Users/ericma/desktop/cryptoScraper/dags/scripts'))
+)
 import scripts.get_price as get_price
 import pandas as pd
 from io import StringIO
@@ -15,34 +18,57 @@ load_dotenv(find_dotenv())
 data = get_price.get_price()
 
 coin_data = {
-    'symbol' : [data[0][0], data[1][0], data[2][0], data[3][0], data[4][0], data[5][0], data[6][0], data[7][0], data[8][0], data[9][0]],
-    'price' : [data[0][1], data[1][1], data[2][1], data[3][1], data[4][1], data[5][1], data[6][1], data[7][1], data[8][1], data[9][1]]
+    "symbol" : [
+        data[0][0], 
+        data[1][0], 
+        data[2][0], 
+        data[3][0], 
+        data[4][0], 
+        data[5][0], 
+        data[6][0], 
+        data[7][0], 
+        data[8][0], 
+        data[9][0]
+    ],
+    "price" : [
+        data[0][1], 
+        data[1][1], 
+        data[2][1], 
+        data[3][1], 
+        data[4][1], 
+        data[5][1], 
+        data[6][1], 
+        data[7][1], 
+        data[8][1], 
+        data[9][1]
+    ],
 }
 
-def upload_to_s3() :
+def upload_to_s3():
     df = pd.DataFrame(coin_data)
-    bucket = 'crypto-webscraper'
+    bucket = "crypto-webscraper"
     csv_buffer = StringIO()
     df.to_csv(csv_buffer)
-    s3_resource = boto3.resource('s3', aws_access_key_id = os.getenv('S3_ACCESS_KEY_ID'), aws_secret_access_key = os.getenv('S3_SECRET_ACCESS_KEY'))
-    s3_resource.Object(bucket, 'testv3.csv').put(Body=csv_buffer.getvalue())
+    s3_resource = boto3.resource(
+        "s3", 
+        aws_access_key_id=os.getenv("S3_ACCESS_KEY_ID"), 
+        aws_secret_access_key=os.getenv("S3_SECRET_ACCESS_KEY")
+    )
+    s3_resource.Object(bucket, "testv3.csv").put(Body=csv_buffer.getvalue())
 
-default_args = {
-    'owner' : 'Eric',
-    'retries' : 5,
-    'retry_delay' : timedelta(minutes = 5)
+default_args = {"owner" : "Eric", "retries" : 5, "retry_delay" : timedelta(minutes = 5)
 }
 
 with DAG(
-    default_args = default_args,
-    dag_id = 'upload_to_s3',
-    description = 'Upload top 10 coins to s3 daily',
-    start_date = datetime(2023 , 1, 11),
-    schedule_interval = '@daily'
+    default_args=default_args,
+    dag_id="upload_to_s3",
+    description="Upload top 10 coins to s3 daily",
+    start_date=datetime(2023 , 1, 11),
+    schedule_interval="@daily"
 ) as dag:
     task1 = PythonOperator(
-        task_id = 'upload_to_s3',
-        python_callable = upload_to_s3,
+        task_id="upload_to_s3",
+        python_callable=upload_to_s3,
     )
     
     task1
